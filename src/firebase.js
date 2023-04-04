27bc82b0a00944c90c2942815f7c79c91327ddd0
Yves Tscherry <info@feerglas.com>
2023-04-04 20:59:15 +0200

    add firebase functions, db, hosting

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAL_brkacNl9ske-uJPaSniKDRhHCi2duc",
+  authDomain: "time-tragg.firebaseapp.com",
+  projectId: "time-tragg",
+  storageBucket: "time-tragg.appspot.com",
+  messagingSenderId: "787248463146",
+  appId: "1:787248463146:web:beb0e39e80997970163253"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
