989323485dc1af396243283f1b553f785583538a
Yves Tscherry <info@feerglas.com>
2023-04-05 19:50:26 +0200

    use an appropriate regex to validate email

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,39 +1,5 @@
-export const isValidMail = (mail) => {
-  if (!mail.includes('@')) {
-    return false;
-  }
-
-  const mailSplit = mail.split('@');
-
-  if (mailSplit.length !== 2) {
-    return false;
-  }
-
-  if (mailSplit[0].length < 1) {
-    return false;
-  }
-
-  const mailAppendix = mailSplit[1];
-
-  if (!mailAppendix.includes('.')) {
-    return false;
-  }
-
-  if (mailAppendix.split('.').length < 2) {
-    return false;
-  }
-
-  if (mailAppendix.split('.')[0].length < 1) {
-    return false;
-  }
-
-  if (mailAppendix.split('.')[1].length < 1) {
-    return false;
-  }
-
-  if (mailAppendix.length < 3) {
-    return false;
-  }
-
-  return true;
-};
+export const isValidMail = (mail) => String(mail)
+  .toLowerCase()
+  .match(
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+  );
