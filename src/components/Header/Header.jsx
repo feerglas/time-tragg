f86bac6fb097517ebab00322aa408c28ac705216
Yves Tscherry <info@feerglas.com>
2023-04-06 19:04:02 +0200

    add basic navigation links

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,32 @@
+import { A, useNavigate } from '@solidjs/router';
+import { fbLogoutUser } from '../../firebase';
+
 function Header() {
+  const navigate = useNavigate();
+
+  const handleLogout = async (evt) => {
+    evt.preventDefault();
+
+    await fbLogoutUser();
+
+    navigate('/login', { replace: true });
+  };
+
   return (
-    <div>Header</div>
+    <ul>
+      <li>
+        <A href="/">add</A>
+      </li>
+      <li>
+        <A href="/list">list</A>
+      </li>
+      <li>
+        <A href="/summary">summary</A>
+      </li>
+      <li>
+        <a href="#" onClick={handleLogout}>logout</a>
+      </li>
+    </ul>
   );
 }
 
