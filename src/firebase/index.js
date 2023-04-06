522a33b51e056f79cd2622abcb8ae8d9e11673ab
Yves Tscherry <info@feerglas.com>
2023-04-06 16:58:20 +0200

    add helpers to signIn user and to get userInfo

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,5 +1,7 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged,
+} from 'firebase/auth';
 import { firebaseConfig } from './config';
 
 // Initialize Firebase
@@ -12,6 +14,37 @@ export const fbSignUpUser = async (email, password) => {
 
     return await createUserWithEmailAndPassword(auth, email, password);
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error.code);
+  }
+};
+
+// Log in existing user
+export const fbSignInUser = async (email, password) => {
+  try {
+    const auth = getAuth();
+
+    return await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    throw new Error(error.code);
+  }
+};
+
+// Get current login state
+export const fbGetLoginState = (cb) => {
+  try {
+    const auth = getAuth();
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        cb(user.uid);
+
+        return;
+      }
+
+      throw new Error('unable to get user info');
+    });
+
+  } catch (error) {
+    throw new Error(error.code);
   }
 };
