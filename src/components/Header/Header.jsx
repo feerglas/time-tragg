24c309d957358eb0e94c11e885101fb2feb08d8a
Yves Tscherry <info@feerglas.com>
2023-04-06 19:16:53 +0200

    only show header if user is logged int

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,22 @@
 import { A, useNavigate } from '@solidjs/router';
-import { fbLogoutUser } from '../../firebase';
+import { createEffect, createSignal, Show } from 'solid-js';
+
+import { fbLogoutUser, fbGetLoginState } from '../../firebase';
+import styles from './Header.module.css';
 
 function Header() {
   const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = createSignal(true);
+
+  createEffect(() => {
+    fbGetLoginState((uid) => {
+      if (!uid) {
+        setLoggedIn(false);
+      } else {
+        setLoggedIn(true);
+      }
+    });
+  });
 
   const handleLogout = async (evt) => {
     evt.preventDefault();
@@ -13,20 +27,22 @@ function Header() {
   };
 
   return (
-    <ul>
-      <li>
-        <A href="/">add</A>
-      </li>
-      <li>
-        <A href="/list">list</A>
-      </li>
-      <li>
-        <A href="/summary">summary</A>
-      </li>
-      <li>
-        <a href="#" onClick={handleLogout}>logout</a>
-      </li>
-    </ul>
+    <Show when={loggedIn()}>
+      <ul>
+        <li>
+          <A activeClass={styles['item--active']} href="/add">add</A>
+        </li>
+        <li>
+          <A activeClass={styles['item--active']} href="/list">list</A>
+        </li>
+        <li>
+          <A activeClass={styles['item--active']} href="/summary">summary</A>
+        </li>
+        <li>
+          <a href="#" onClick={handleLogout}>Logout</a>
+        </li>
+      </ul >
+    </Show>
   );
 }
 
