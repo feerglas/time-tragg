4bc0a65127e6ef3ca9969e82299714c27876ebdc
Yves Tscherry <info@feerglas.com>
2023-04-07 11:55:10 +0200

    add dateTime picker component

--- src/pages/Add/Add.jsx
+++ src/pages/Add/Add.jsx
@@ -1,6 +1,35 @@
+import { createSignal, createEffect } from 'solid-js';
+import { Button } from '../../components/Button/Button.jsx';
+import { DateTimePicker } from '../../components/DateTimePicker/DateTimePicker.jsx';
+
 function Add() {
+  const [selection, setSelection] = createSignal({});
+
+  createEffect(() => {
+    console.log(selection());
+  });
+
+  const onSubmit = async (evt) => {
+    evt.preventDefault();
+  };
+
   return (
-    <div>Add</div>
+    <div>
+      <h1>Add an entry</h1>
+
+      <form onSubmit={onSubmit}>
+        <DateTimePicker
+          handleSelect={setSelection}
+        />
+
+        <Button
+          type="submit"
+          text="Save"
+          disabled={!selection()}
+        />
+      </form>
+
+    </div>
   );
 }
 
