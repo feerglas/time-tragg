15648d099f959877687f2d23636e24877f942384
Yves Tscherry <info@feerglas.com>
2023-04-08 16:39:56 +0200

    add pagination

--- src/components/EntriesTable/EntriesTable.jsx
+++ src/components/EntriesTable/EntriesTable.jsx
@@ -1,44 +1,121 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import { Table as BootstrapTable } from 'solid-bootstrap';
-import { For } from 'solid-js';
+import { Table as BootstrapTable, Pagination, Form } from 'solid-bootstrap';
+import {
+  For, createEffect, createSignal, createMemo, Show,
+} from 'solid-js';
 import { Button } from '../Button/Button.jsx';
 import { dbKeys } from '../../firebase/keys';
 import { getDayForDisplay, getDateForDisplay } from '../../helpers/dateTime';
 
+const config = {
+  itemsPerPage: 5,
+};
+
 function EntriesTable(props) {
+  const [currentPage, setCurrentPage] = createSignal(0);
+  const [currentItems, setCurrentItems] = createSignal([]);
+  const totalPages = createMemo(() => {
+    if (props.entries()) {
+      const divider = props.entries().length / config.itemsPerPage;
+      let pages = Math.floor(divider);
+
+      if (divider - Math.floor(divider) !== 0) {
+        pages += 1;
+      }
+
+      return pages;
+    }
+
+    return [];
+  });
+
+  createEffect(() => {
+    if (!props.entries()) {
+      return;
+    }
+
+    const startIndex = currentPage() * config.itemsPerPage;
+    const endIndex = startIndex + config.itemsPerPage;
+    const newItems = props.entries().slice(startIndex, endIndex);
+
+    setCurrentItems(newItems);
+  });
+
+  const handlePaginationSelect = (evt) => {
+    const val = parseInt(evt.target.value, 10);
+
+    setCurrentPage(val);
+
+  };
+
+  const handlePaginationNext = () => {
+    if (currentPage() > totalPages() - 2) {
+      return;
+    }
+
+    setCurrentPage(currentPage() + 1);
+  };
+
+  const handlePaginationPrevious = () => {
+    if (currentPage() < 1) {
+      return;
+    }
+
+    setCurrentPage(currentPage() - 1);
+  };
+
   return (
-    <BootstrapTable striped bordered hover size="sm">
-      <thead>
-        <tr>
-          <th>Date</th>
-          <th>Duration</th>
-          <th />
-        </tr>
-      </thead>
-      <tbody>
-        <For each={props.entries()}>
-          {(entry) => (
-            <tr>
-              <td>
-                <span>{getDayForDisplay(entry[dbKeys.date])}</span>
-                <span>{getDateForDisplay(entry[dbKeys.date])}</span>
-              </td>
-              <td>
-                <span>{entry[dbKeys.startTime]}</span>
-                <span>-</span>
-                <span>{entry[dbKeys.endTime]}</span>
-              </td>
-              <td>
-                <Button
-                  text="delete"
-                  onClick={props.handleDelete(entry.id)}
-                />
-              </td>
-            </tr>
-          )}
-        </For>
-      </tbody>
-    </BootstrapTable>
+    <div>
+      <Show when={totalPages() > 0}>
+        <Pagination>
+          <Pagination.Prev onClick={handlePaginationPrevious} />
+          <Form.Select
+            onChange={handlePaginationSelect}
+            value={currentPage()}
+          >
+            <For each={[...Array(totalPages()).keys()]}>
+              {(page) => (
+                <option value={page}>{page + 1}</option>
+              )}
+            </For>
+          </Form.Select>
+          <Pagination.Next onClick={handlePaginationNext} />
+        </Pagination>
+      </Show>
+
+      <BootstrapTable striped bordered hover size="sm">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Duration</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>
+          <For each={currentItems()}>
+            {(entry) => (
+              <tr>
+                <td>
+                  <span>{getDayForDisplay(entry[dbKeys.date])}</span>
+                  <span>{getDateForDisplay(entry[dbKeys.date])}</span>
+                </td>
+                <td>
+                  <span>{entry[dbKeys.startTime]}</span>
+                  <span>-</span>
+                  <span>{entry[dbKeys.endTime]}</span>
+                </td>
+                <td>
+                  <Button
+                    text="delete"
+                    onClick={props.handleDelete(entry.id)}
+                  />
+                </td>
+              </tr>
+            )}
+          </For>
+        </tbody>
+      </BootstrapTable>
+    </div>
   );
 }
 
