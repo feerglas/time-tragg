37860ca5bac4b699becdf35580a5032e3fbdf2c7
Yves Tscherry <info@feerglas.com>
2023-04-08 09:51:22 +0200

    remove route guard

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Add } from './pages/Add/Add.jsx';
 import { List } from './pages/List/List.jsx';
 import { Summary } from './pages/Summary/Summary.jsx';
 import { Header } from './components/Header/Header.jsx';
-import { RouteGuard } from './components/RouteGuard/RouteGuard.jsx';
 
 import styles from './App.module.css';
 
@@ -17,11 +16,9 @@ function App() {
       <Routes>
         <Route path='/login' component={SignInSignUp} />
         <Route path='/forgot-password' component={ForgotPassword} />
-        <Route path='/' component={RouteGuard}>
-          <Route path='/add' component={Add} />
-          <Route path='/list' component={List} />
-          <Route path='/summary' component={Summary} />
-        </Route>
+        <Route path='/add' component={Add} />
+        <Route path='/list' component={List} />
+        <Route path='/summary' component={Summary} />
         <Route path="*" element={() => <div>Page not found</div>} />
       </Routes>
     </div>
