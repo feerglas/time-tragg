4e01640c9f1063dae945a1b48a1879fa63f0cfe3
Yves Tscherry <info@feerglas.com>
2023-04-08 11:25:44 +0200

    add methods to check if an entry is already there before adding a new one

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -1,13 +1,79 @@
 import { initializeApp } from 'firebase/app';
 import {
-  getDatabase, ref, child, push, update,
+  getDatabase, ref, child, push, update, get,
 } from 'firebase/database';
 import { firebaseConfig } from './config';
 import { dbKeys } from './keys';
+import { arrayifyFirebaseObject } from '../helpers/dataTransform';
+import { formatDateStringForDateComparison, removeColonsFromTime } from '../helpers/dateTime';
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const dbRef = ref(database);
+
+const getAllEntries = async (uid) => {
+  try {
+    const snapshot = await get(child(dbRef, `${uid}/${dbKeys.workouts}`));
+
+    if (snapshot.exists()) {
+      return snapshot.val();
+    }
+
+    throw new Error('Error in getAllEntries');
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const getAllEntriesForDateRange = async (uid, from, to) => {
+  const allEntries = await getAllEntries(uid);
+  const allEntriesArray = arrayifyFirebaseObject(allEntries);
+  const fromFormatted = formatDateStringForDateComparison(from);
+  const toFormatted = formatDateStringForDateComparison(to);
+  const filteredEntries = allEntriesArray.filter((item) => {
+    const entryDate = new Date(item[dbKeys.date]);
+    const entryDateFormatted = formatDateStringForDateComparison(entryDate);
+
+    return entryDateFormatted <= toFormatted && entryDateFormatted >= fromFormatted;
+  });
+
+  return filteredEntries;
+};
+
+const entryAlreadyThere = async (entry, uid) => {
+  const date = entry[dbKeys.date];
+  const entriesForDate = await getAllEntriesForDateRange(uid, date, date);
+
+  if (entriesForDate.length === 0) {
+    return false;
+  }
+
+  const from = parseInt(removeColonsFromTime(entry[dbKeys.startTime]), 10);
+  const to = parseInt(removeColonsFromTime(entry[dbKeys.endTime]), 10);
+
+  let itemFound = false;
+
+  entriesForDate.every((item) => {
+    const itemFrom = parseInt(removeColonsFromTime(item[dbKeys.startTime]), 10);
+    const itemTo = parseInt(removeColonsFromTime(item[dbKeys.endTime]), 10);
+
+    if (itemFrom >= from && itemFrom <= to) {
+      itemFound = true;
+
+      return false;
+    }
+
+    if (itemTo <= to && itemTo >= from) {
+      itemFound = true;
+
+      return false;
+    }
+
+    return true;
+  });
+
+  return itemFound;
+};
 
 export const addEntry = async (uid, data) => {
   const path = `${uid}/${dbKeys.workouts}`;
@@ -18,10 +84,16 @@ export const addEntry = async (uid, data) => {
   updates[updateKey] = data;
 
   try {
+    const alreadyThere = await entryAlreadyThere(data, uid);
+
+    if (alreadyThere) {
+      throw new Error('There is already an entry in this time range.');
+    }
+
     await update(dbRef, updates);
 
     return true;
-  } catch {
-    return false;
+  } catch (error) {
+    throw new Error(error);
   }
 };
