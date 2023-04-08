8f1b09b0626b86a1d117d4848da70c87dc27c138
Yves Tscherry <info@feerglas.com>
2023-04-08 14:02:20 +0200

    format date

--- src/components/EntriesTable/EntriesTable.jsx
+++ src/components/EntriesTable/EntriesTable.jsx
@@ -3,6 +3,7 @@ import { Table as BootstrapTable } from 'solid-bootstrap';
 import { For } from 'solid-js';
 import { Button } from '../Button/Button.jsx';
 import { dbKeys } from '../../firebase/keys';
+import { getDayForDisplay, getDateForDisplay } from '../../helpers/dateTime';
 
 function EntriesTable(props) {
   return (
@@ -18,8 +19,15 @@ function EntriesTable(props) {
         <For each={props.entries()}>
           {(entry) => (
             <tr>
-              <td>{entry[dbKeys.date]}</td>
-              <td>{entry[dbKeys.startTime]} - {entry[dbKeys.endTime]}</td>
+              <td>
+                <span>{getDayForDisplay(entry[dbKeys.date])}</span>
+                <span>{getDateForDisplay(entry[dbKeys.date])}</span>
+              </td>
+              <td>
+                <span>{entry[dbKeys.startTime]}</span>
+                <span>-</span>
+                <span>{entry[dbKeys.endTime]}</span>
+              </td>
               <td>
                 <Button
                   text="delete"
