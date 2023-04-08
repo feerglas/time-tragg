126451decbfc483479e6b70b034ffb8dade670c1
Yves Tscherry <info@feerglas.com>
2023-04-08 12:00:16 +0200

    list all entries

--- src/pages/List/List.jsx
+++ src/pages/List/List.jsx
@@ -1,6 +1,24 @@
+import { createResource, For } from 'solid-js';
+import { getAllEntries } from '../../firebase/db';
+import { useUid } from '../../components/UidProvider/UidProvider.jsx';
+import { dbKeys } from '../../firebase/keys';
+
 function List() {
+  const [uid] = useUid();
+  const [entries] = createResource(uid, getAllEntries);
+
   return (
-    <div>List</div>
+    <div>
+      <ul>
+        <For each={entries()} fallback={<div>Loading...</div>}>
+          {(entry) => (
+            <li>
+              {entry[dbKeys.date]}: {entry[dbKeys.startTime]} - {entry[dbKeys.endTime]}
+            </li>
+          )}
+        </For>
+      </ul>
+    </div>
   );
 }
 
