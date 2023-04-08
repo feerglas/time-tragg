f2ca00cf06fdd40d52d9b6498eaa33e20048f09a
Yves Tscherry <info@feerglas.com>
2023-04-08 13:37:29 +0200

    add onclick handler

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,9 @@
 export function Button(props) {
   return (
-    <button disabled={props.disabled} type={props.type}>{props.text}</button>
+    <button
+      disabled={props.disabled}
+      type={props.type}
+      onClick={() => props.onClick()}
+    >{props.text}</button>
   );
 }
