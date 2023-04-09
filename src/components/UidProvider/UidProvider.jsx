e4c7590a828ac55da333557674a7133def5265d0
Yves Tscherry <info@feerglas.com>
2023-04-09 18:05:58 +0200

    add email to provider

--- src/components/UidProvider/UidProvider.jsx
+++ src/components/UidProvider/UidProvider.jsx
@@ -9,20 +9,23 @@ const UidContext = createContext();
 export function UidProvider(props) {
   const navigate = useNavigate();
   const [uid, setUid] = createSignal(false);
+  const [email, setEmail] = createSignal(false);
 
   createEffect(() => {
     fbGetLoginState((identifier) => {
       if (identifier) {
         setUid(identifier.uid);
+        setEmail(identifier.email);
       } else {
         setUid(false);
+        setEmail(false);
         navigate('/login', { replace: true });
       }
     });
   });
 
   return (
-    <UidContext.Provider value={[uid]}>
+    <UidContext.Provider value={[uid, email]}>
       {props.children}
     </UidContext.Provider>
   );
