a4ec9200c83382b429f6401c87e89a87b948118f
Yves Tscherry <info@feerglas.com>
2023-04-09 17:57:00 +0200

    add icon option

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,13 +1,21 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Button as BootstrapButton } from 'solid-bootstrap';
+import styles from './Button.module.scss';
+
 export function Button(props) {
+  const handleClick = () => {
+    if (props.onClick) {
+      props.onClick();
+    }
+  };
+
   return (
-    <button
+    <BootstrapButton
+      variant={props.variant}
       disabled={props.disabled}
       type={props.type}
-      onClick={() => {
-        if (props.onClick) {
-          props.onClick();
-        }
-      }}
-    >{props.text}</button>
+      onClick={handleClick}
+      class={styles.button}
+    >{props.text}{props.icon}</BootstrapButton>
   );
 }
